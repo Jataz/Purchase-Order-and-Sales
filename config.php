31fa6352e8bb0eaf153f395601a21f86233e044b
jataz <africajataz@gmail.com>
2025-01-20 21:35:01 +0200

    Database

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-ini_set('date.timezone','Asia/Manila');
-date_default_timezone_set('Asia/Manila');
+ini_set('date.timezone','Africa/Harare');
+date_default_timezone_set('Africa/Harare');
 session_start();
 
 require_once('initialize.php');
